321cfeda4058be32ea10e71e67bdd045a4364787
Lisc <qqwer153@gmail.com>
2022-04-15 05:37:06 +0900

    fix : PlayerTest bug fix

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -3,6 +3,8 @@ package baseball;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import baseball.domain.Player;
 import baseball.util.NumberUtils;
@@ -15,28 +17,27 @@ import java.util.List;
 
 class PlayerTest {
     Player player = null;
-    List<Integer> numbers = null;
     
     @BeforeEach
     void init () {
         player = new Player();
-        numbers = new ArrayList<Integer>();
-        
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
     }
     
     @Test
     @DisplayName("setNumber size 테스트")
-    void setNumber_size_test(String input) {
+    void setNumber_size_test() {
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
         player.setNumbers(numbers);
 
         assertThat(numbers.size()).isEqualTo(player.getNumbers().size());
     }
-
-    @Test
-    @DisplayName("setNumber 값 비교 테스트")
+    
+    @ParameterizedTest
+    @DisplayName("splitNum 값 비교 테스트")
+    @ValueSource(strings = {"123","456","789"})
     void setNumber_equal_test(String input) {
         List<Integer> numbers = NumberUtils.splitNum(input);
         player.setNumbers(numbers);
