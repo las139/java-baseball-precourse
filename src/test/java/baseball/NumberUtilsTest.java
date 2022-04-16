8acb7c39cf41fd515340d11112ea0278a27207aa
Lisc <qqwer153@gmail.com>
2022-04-16 21:38:42 +0900

    rename : NumberTest to NumberUtilsTest

--- src/test/java/baseball/NumberUtilsTest.java
+++ src/test/java/baseball/NumberUtilsTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
-class NumberTest {
+class NumberUtilsTest {
     
     @Test
     @DisplayName("랜덤 번호 생성 범위 1 ~ 9 테스트")
